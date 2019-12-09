ec2dec9d2019b655e822e9669001060021f21948
TheBestPessimist <cristian@tbp.land>
2019-12-09 20:13:39 +0200

    Add a build script for easier start/stop

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+docker stop telegram_bot_kt
+docker rm telegram_bot_kt
+
+docker build -t telegram_bot_kt --no-cache .
+
+# this is used for production
+docker run -dt --restart on-failure -p 13337:13337 --name telegram_bot_kt telegram_bot_kt
+
+# this is used when testing as it attaches to the console
+#docker run -p 13337:13337 -m80m --name telegram_bot_kt telegram_bot_kt
