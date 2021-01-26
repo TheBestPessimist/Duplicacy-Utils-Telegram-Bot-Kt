aae3ad54ed93f25442f144db6b6930b0694031d0
TheBestPessimist <cristian@tbp.land>
2021-01-26 13:22:19 +0200

    Fix build + bump versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,9 +4,8 @@ description = "Telegram Bot used for my Duplicacy Utils scripts"
 java.sourceCompatibility = JavaVersion.VERSION_15
 java.targetCompatibility = JavaVersion.VERSION_15
 
-val ktor_version = "1.2.6"
-val kotlin_version = "1.3.61"
-val jackson_datatype_version = "2.9.9"
+val ktorVersion = "1.5.1"
+val jacksonVersion = "2.12.1"
 val logbackVersion = "1.2.3"
 
 
@@ -23,23 +22,23 @@ repositories {
 
 
 dependencies {
-    compile("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version")
-    compile("io.ktor:ktor-server-netty:$ktor_version")
-    compile("ch.qos.logback:logback-classic:$logbackVersion")
-    compile("io.ktor:ktor-client-core:$ktor_version")
-    compile("io.ktor:ktor-client-core-jvm:$ktor_version")
-    compile("io.ktor:ktor-client-json-jvm:$ktor_version")
-    compile("io.ktor:ktor-client-jackson:$ktor_version")
-    compile("io.ktor:ktor-client-logging-jvm:$ktor_version")
-    compile("io.ktor:ktor-server-core:$ktor_version")
-    compile("io.ktor:ktor-jackson:$ktor_version")
-    compile("io.ktor:ktor-metrics:$ktor_version")
-    compile("io.ktor:ktor-server-host-common:$ktor_version")
-    compile("io.ktor:ktor-client-apache:$ktor_version")
-    compile("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jackson_datatype_version")
-    testCompile("io.ktor:ktor-server-tests:$ktor_version")
-    testCompile("io.ktor:ktor-client-mock:$ktor_version")
-    testCompile("io.ktor:ktor-client-mock-jvm:$ktor_version")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
+    implementation("io.ktor:ktor-client-core-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-client-json-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-client-jackson:$ktorVersion")
+    implementation("io.ktor:ktor-client-logging-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-jackson:$ktorVersion")
+    implementation("io.ktor:ktor-metrics:$ktorVersion")
+    implementation("io.ktor:ktor-server-host-common:$ktorVersion")
+    implementation("io.ktor:ktor-client-apache:$ktorVersion")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
+    testImplementation("io.ktor:ktor-server-tests:$ktorVersion")
+    testImplementation("io.ktor:ktor-client-mock:$ktorVersion")
+    testImplementation("io.ktor:ktor-client-mock-jvm:$ktorVersion")
 
     configurations.all {
         exclude(group = "junit", module = "junit")
